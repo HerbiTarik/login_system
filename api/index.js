cd8b6c2d22a1ae5c9d2721c5dcb73f46e14e7877
Tarik <herbitarik2509@gmail.com>
2023-10-28 21:46:25 +0200

    Initialization of the RN application and creation of api (Back-end folder)

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 3000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.json());
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect("mongodb+srv://tarik:tarik@cluster0.kpmllcs.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error Connecting to MongoDB");
+  });
+
+app.listen(port, () => {
+  console.log("server is running on port 3000");
+});
