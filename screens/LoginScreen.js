6bedac45d8f390cdfdc5b064fe05562c9506b102
Tarik <herbitarik2509@gmail.com>
2023-10-29 00:57:39 +0200

    Add StackNavigator and screens

---
+++ screens/LoginScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+});
+
+export default LoginScreen;
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const RegisterScreen = () => {
+  return (
+    <View>
+      <Text>RegisterScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default RegisterScreen;
